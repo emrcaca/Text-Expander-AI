d613c48009be55c1d892f25e1f9a36e3863364c3
Emir <emrcaca@yahoo.com>
2025-12-12 15:09:03 +0300

    Update Userscript.js

--- Userscript.js
+++ Userscript.js
@@ -674,7 +674,7 @@
         }
 
         .te-settings__textarea {
-            width: 100%;
+            width: 96%;
             min-height: 120px;
             background: rgba(255, 255, 255, 0.05);
             border: 1px solid rgba(255, 255, 255, 0.1);
